805486f5075cf34ce38e4c3cd8ca0749310d9d1f
Viacheslav <freefly1911@gmail.com>
2019-11-13 12:54:54 +0200

    Это наш первый коммит

---
+++ 1 variabels & other data/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var block = "bricks";
+var quantity = 10;
+
+let persone = {
+    name: 'john',
+    age: 32,
+    isMarried: false
+};
+
+let arr = ['plum.png','apple.jpeg','orange.bmp'];
+
+
+console.log(persone['name']);
+
+console.log(arr[0]);
+
+alert("Hello World");
+
+// let answer = confirm("вам есть 18?");
+
+// console.log(answer);
+
+let answer = prompt("вам есть 18?", "Да");
+
+console.log(answer);
+console.log(typeof(answer));
